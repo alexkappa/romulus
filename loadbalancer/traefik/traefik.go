48dc810a749dd6c617559d92d8eedb67012d2103
albertrdixon <albert@dixon.rocks>
2016-01-14 15:08:26 -0800

    update deps and add store implementation

--- loadbalancer/traefik/traefik.go
+++ loadbalancer/traefik/traefik.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/albertrdixon/gearbox/logger"
 	"github.com/emilevauge/traefik/types"
-	"github.com/timelinelabs/pkg/store"
 	"github.com/timelinelabs/romulus/kubernetes"
 	"github.com/timelinelabs/romulus/loadbalancer"
 )
@@ -36,28 +35,17 @@ const (
 )
 
 func New(prefix string, peers []string, timeout time.Duration, ctx context.Context) (*traefik, error) {
-	etcd, er := store.NewEtcdStore(peers, timeout)
+	st, er := NewEtcdStore(peers, timeout)
 	if er != nil {
 		return nil, er
 	}
 	t := &traefik{
 		prefix:  prefix,
 		Context: ctx,
-		Store:   Store{etcd},
+		Store:   st,
 	}
-	if er := t.Status(); er != nil {
-		return nil, er
-	}
-	if keys, er := t.GetMultiMap(prefix); er != nil {
-		frontends := path.Join(prefix, "frontends")
-		backends := path.Join(prefix, "backends")
-		if _, ok := keys[frontends]; !ok {
-			t.Set(frontends, []byte(""), 0)
-		}
-		if _, ok := keys[backends]; !ok {
-			t.Set(backends, []byte(""), 0)
-		}
-	}
+	t.Mkdir(path.Join(prefix, "frontends"))
+	t.Mkdir(path.Join(prefix, "backends"))
 	return t, nil
 }
 
@@ -66,8 +54,7 @@ func (t *traefik) Kind() string {
 }
 
 func (t *traefik) Status() error {
-	_, er := t.Keys("/")
-	return er
+	return t.Exists("/")
 }
 
 func (t *traefik) NewFrontend(svc *kubernetes.Service) (loadbalancer.Frontend, error) {
@@ -93,11 +80,11 @@ func (t *traefik) UpsertFrontend(fr loadbalancer.Frontend) error {
 	}
 
 	pre := path.Join(t.prefix, "frontends", fr.GetID())
-	if er := t.Set(path.Join(pre, "backend"), []byte(f.Backend), 0); er != nil {
+	if er := t.Set(path.Join(pre, "backend"), f.Backend); er != nil {
 		return fmt.Errorf("Upsert %v failed: %v", fr, er)
 	}
 	if f.PassHostHeader {
-		if er := t.Set(path.Join(pre, phh), []byte("true"), 0); er != nil {
+		if er := t.Set(path.Join(pre, phh), "true"); er != nil {
 			logger.Warnf("[%v] Upsert %s error: %v", fr.GetID(), phh, er)
 		}
 	}
@@ -106,10 +93,10 @@ func (t *traefik) UpsertFrontend(fr loadbalancer.Frontend) error {
 		logger.Debugf("[%v] Adding Route(%s=%q)", fr.GetID(), rt.Rule, rt.Value)
 		ruleK := path.Join(pre, "routes", id, "rule")
 		valk := path.Join(pre, "routes", id, "value")
-		if er := t.Set(ruleK, []byte(rt.Rule), 0); er != nil {
+		if er := t.Set(ruleK, rt.Rule); er != nil {
 			logger.Warnf("[%v] Upsert rule error: %v", fr.GetID(), er)
 		}
-		if er := t.Set(valk, []byte(rt.Value), 0); er != nil {
+		if er := t.Set(valk, rt.Value); er != nil {
 			logger.Warnf("[%v] Upsert value error: %v", fr.GetID(), er)
 		}
 	}
@@ -119,7 +106,7 @@ func (t *traefik) UpsertFrontend(fr loadbalancer.Frontend) error {
 func (t *traefik) DeleteFrontend(fr loadbalancer.Frontend) error {
 	logger.Debugf("[%v] Attempting to delete: %v", fr.GetID(), fr)
 	key := path.Join(t.prefix, "frontends", fr.GetID())
-	return t.Delete(key, true)
+	return t.Delete(key)
 }
 
 func (t *traefik) NewBackend(svc *kubernetes.Service) (loadbalancer.Backend, error) {
@@ -147,12 +134,12 @@ func (t *traefik) UpsertBackend(ba loadbalancer.Backend) error {
 
 	pre := path.Join(t.prefix, "backends", ba.GetID())
 	if b.CircuitBreaker != nil && b.CircuitBreaker.Expression != "" {
-		if er := t.Set(path.Join(pre, cb), []byte(b.CircuitBreaker.Expression), 0); er != nil {
+		if er := t.Set(path.Join(pre, cb), b.CircuitBreaker.Expression); er != nil {
 			logger.Warnf("[%v] Upsert %s error: %v", ba.GetID(), cb, er)
 		}
 	}
 	if b.LoadBalancer != nil && b.LoadBalancer.Method != "" {
-		if er := t.Set(path.Join(pre, lb), []byte(b.LoadBalancer.Method), 0); er != nil {
+		if er := t.Set(path.Join(pre, lb), b.LoadBalancer.Method); er != nil {
 			logger.Warnf("[%v] Upsert %s error: %v", ba.GetID(), lb, er)
 		}
 	}
@@ -161,11 +148,11 @@ func (t *traefik) UpsertBackend(ba loadbalancer.Backend) error {
 		logger.Debugf("[%v] Upserting Server(%v)", ba.GetID(), srv.URL)
 		urlK := path.Join(pre, "servers", id, "url")
 		weightK := path.Join(pre, "servers", id, "weight")
-		if er := t.Set(urlK, []byte(srv.URL), 0); er != nil {
+		if er := t.Set(urlK, srv.URL); er != nil {
 			logger.Warnf("[%v] Upsert error: %v", ba.GetID(), er)
 		}
 		weight := strconv.Itoa(srv.Weight)
-		if er := t.Set(weightK, []byte(weight), 0); er != nil {
+		if er := t.Set(weightK, weight); er != nil {
 			logger.Warnf("[%v] Upsert error: %v", ba.GetID(), er)
 		}
 	}
@@ -175,7 +162,7 @@ func (t *traefik) UpsertBackend(ba loadbalancer.Backend) error {
 func (t *traefik) DeleteBackend(ba loadbalancer.Backend) error {
 	logger.Debugf("[%v] Attempting delete: %v", ba.GetID(), ba)
 	key := path.Join(t.prefix, "backends", ba.GetID())
-	return t.Delete(key, true)
+	return t.Delete(key)
 }
 
 func (t *traefik) NewServers(svc *kubernetes.Service) ([]loadbalancer.Server, error) {
@@ -198,11 +185,11 @@ func (t *traefik) UpsertServer(ba loadbalancer.Backend, srv loadbalancer.Server)
 
 	urlKey := path.Join(t.prefix, "backends", ba.GetID(), "servers", srv.GetID(), "url")
 	weightKey := path.Join(t.prefix, "backends", ba.GetID(), "servers", srv.GetID(), "weight")
-	if er := t.Set(urlKey, []byte(srv.(*server).URL), 0); er != nil {
+	if er := t.Set(urlKey, srv.(*server).URL); er != nil {
 		return er
 	}
 	weight := strconv.Itoa(srv.(*server).Weight)
-	if er := t.Set(weightKey, []byte(weight), 0); er != nil {
+	if er := t.Set(weightKey, weight); er != nil {
 		return er
 	}
 	return nil
@@ -215,7 +202,7 @@ func (t *traefik) DeleteServer(ba loadbalancer.Backend, srv loadbalancer.Server)
 		return fmt.Errorf("Lookup %v failed: %v", srv, er)
 	}
 
-	return t.Delete(key, true)
+	return t.Delete(key)
 }
 
 func (t *traefik) NewMiddlewares(svc *kubernetes.Service) ([]loadbalancer.Middleware, error) {
